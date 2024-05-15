1d689b1a629f2e2f8062205f6b71f98135e17285
dkittleson <dkittles>
2024-05-15 12:36:28 -0600

    count words in book

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 def main():
     book_path = "books/frankestein.txt"
     text = get_text(book_path)
-    print(text)
+    split = split_text(text)
+    count = count_text(split)
+    print(count)
 
 def get_text(path):
     with open(path) as f:
         return f.read()
+
+def split_text(texts):
+    text = texts.split()
+    return text
+
+def count_text(texts):
+    i = 0
+    for text in texts:
+        i += 1
+    return i
+
 
 main()
