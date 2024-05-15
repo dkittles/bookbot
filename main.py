250dbacf496562151918b3619ced4890148ab7fc
dkittleson <dkittles>
2024-05-14 22:43:09 -0600

    fixed main.py with functions

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-with open(github.com/dkittles/bookbot/books/frankestein.txt) as f:
-    file_contents = f.read()
+def main():
+    book_path = "books/frankestein.txt"
+    text = get_text(book_path)
+    print(text)
+
+def get_text(path):
+    with open(path) as f:
+        return f.read()
+
 main()
