3ea7eec84b86d5fa01bdefc00dcd98d95c07681a
dkittleson <dkittles>
2024-05-17 22:15:15 -0600

    added dict, key = letter, value = #

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ def main():
     book_path = "books/frankestein.txt"
     text = get_text(book_path)
     split = split_text(text)
-
-    print(len(split))
+    words = count_letters(split)
+    print(words)
 
 def get_text(path):
     with open(path) as f:
@@ -13,5 +13,14 @@ def split_text(texts):
     text = texts.split()
     return text
 
-
+def count_letters(words):
+    alpha_dict = {}
+    for word in words:
+        for letter in word:
+            letter = letter.lower()
+            if not letter in alpha_dict:
+                alpha_dict[letter] = 1
+            else:
+                alpha_dict[letter] += 1
+    return alpha_dict
 main()
